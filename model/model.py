b77a272a47ef210353f9a2a39066c0cc193d9d96
Brandon Wingard <56277974+AegisSSC@users.noreply.github.com>
2025-01-02 13:52:23 -0500

    Added the beginnings of a Monte Carlo Estimation Model

--- model/model.py
+++ model/model.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import statistics
+
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -64,3 +66,54 @@ def main():
         X_train, X_test, Y_train, Y_test , X_lately = prepare_data(dataset,forecast_col,forecast_out,test_size); 
         forecast, score = generate_model(X_train, X_test, Y_train, Y_test, X_lately) 
         print_response(forecast, score)
+
+
+# Finding a "Buy" and "Sell" signals by finding the rolling 30, 90, and 120 day moving average. 
+# Comparing the Current value to see if it is within 5%, 10%, or 20% of the 52 week low. 
+
+days = (30, 90, 120)
+signal_markers = (0.05, 0.10, 0.25)
+
+def find_annual_low(dataframe):
+    current_lowest = dataframe[0]
+    for day in range(0, len(dataframe)):
+        if current_lowest > day:
+            current_lowest = day
+    return current_lowest
+
+def find_annual_high(dataframe):
+    current_highest = dataframe[0]
+    for day in range(0, len(dataframe)):
+        if current_highest < day:
+            current_highest = day
+    return current_highest
+
+
+
+def find_band(dataframe):
+    current_low = find_annual_low(dataframe['Open'])
+    current_high = find_annual_high(dataframe['Open'])
+
+    smallBandHigh = current_high * (1 - signal_markers[0]) 
+    medBandHigh   = current_high * (1 - signal_markers[1])
+    largeBandHigh = current_high * (1 - signal_markers[2])
+    high_bands = (smallBandHigh, medBandHigh, largeBandHigh)
+
+    smallBandLow = current_low * (1 + signal_markers[0]) 
+    medBandLow   = current_low * (1 + signal_markers[1])
+    largeBandLow = current_low * (1 + signal_markers[2])
+    low_bands = (smallBandLow, medBandLow, largeBandLow)
+
+    return (high_bands, low_bands)
+
+
+
+def find_moving_average(dataframe, moving_range):
+    # moving_range = 30
+    averagesList = []
+    for i in range(0, len(dataframe)):
+        if  moving_range + i < len(dataframe):  
+            frame = dataframe[i : moving_range + i]
+            movingAverage = statistics.mean(frame)
+            averagesList.append(movingAverage)
+            print("The " str(moving_range) + " Day moving average is: " + str(movingAverage))
